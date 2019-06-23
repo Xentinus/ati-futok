0095c092ee9629362462ab26d79fcd1962547591
Béla Kellner <xentinus95@gmail.com>
2019-06-23 10:08:41 +0200

    Lementett változat

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ati-futok
+
+Küldök egy ábrát hogy miszerint kell szerintem ellenörizni, nekem igy kijöttek azok a Futo.Ki-k amik a lapon voltak
+
+![Színes leírás](./assets/szinek.png)
+
+## Végső ellenőrzésnél
+
+Emellett még ellenörzéskor ellenörizni is kell hogy a távolság 0 felett van e, de ha nem teszed az se nagy baj mert majdnem ugyan az lesz az eredmény csak mivel a 4. futó páratlannal kezdett igy bemegy -1-re és akkor beirja még a 41. időpontot is (akkor ér be)
+
+## Miért multidimenziós tömb
+
+[ hol tart, mennyit megy, hányadik futó ]
+
+Ha van változás és azután sorba rendezem tudni fogja hogy az adott sorból mennyit kell levennie és hogy ő eredetileg hányadik futó mert ahogy mutattam fontos tudni hogy hányadikként indult mert akik távolról beelöztek egy közelebbit csak akkor kell kiirni az időpontot
+
+## Végeredmény
+
+| Időpontok  | Távolság | Ki elözőtt be kit | Szövegesen |
+|--------------|---------|----------|----------------------|--------------------------------|
+| Időpont:  4  | 72 < 73 | 2. futó  <  4 . futó | ( 2. futó beelőzte a 4. futót) |
+| Időpont:  4  | 72 < 73 | 3. futó  <  4 . futó | ( 3. futó beelőzte a 4. futót) |
+| Időpont:  5  | 66 < 68 | 2. futó   <  3. futó | ( 2. futó beelőzte a 3. futót) |
+| Időpont:  7  | 65 < 67 | 1. futó  <  4. futó  | ( 1. futó beelőzte a 4. futót) |
+| Időpont:  13 | 35 < 36 | 1. futó  <  3. futó  | ( 1. futó beelőzte a 3. futót) |
---
+++ src/index.js
@@ -0,0 +1,107 @@
+﻿/* ---------- BEVITEL ---------- */
+
+// Futo.Be első sora, kimented mennyi futó van
+let futokMennyisege = 5;
+
+// bemented egy több dimenziós tömbbe a Futo.Be első sora utánniakat
+let futok = [
+  // [ távolság, sebesség, hányadik futó]
+  [100, 5, 1],
+  [96, 6, 2],
+  [88, 4, 3],
+  [81, 2, 4],
+  [10, 1, 5]
+]
+
+/* ----------  ELLENŐRZÉSEK ( KEZDÉSHEZ ) ---------- */
+
+/*
+if (futok.length != futokMennyisege) return;
+
+// FutoBE szabályoknak megfelel
+for (let i = 0; i < futok.length; i++) {
+
+  // max 1 millios távolság
+  if (futok[i][0] >= 1000000) return;
+
+  // csökkenő távolság ahogy indulnak
+  if (futok[i + 1] != undefined) {
+    if (futok[i][0] <= futok[i + 1][0]) return;
+  }
+
+  // távolság több mint 0
+  if (futok[i][0] <= 0) return;
+
+  // sebesség több mint nulla
+  if (futok[i][1] <= 0) return;
+}
+*/
+
+/* ---------- FUTÁS ---------- */
+
+// Időpont és Futo.Ki
+let idopont = 0;
+let futoKi = '';
+
+// Aktuális futás megjelenítése
+aktualisFutas(idopont, futok);
+
+// Körönkénti levétele
+while (mindekiBeer(futok)) {
+  for (const i in futok) {
+    if (futok[i][0] > 0) {
+      futok[i][0] -= futok[i][1];
+    }
+  }
+  // Aktuális futás megjelenítése
+  idopont++;
+  aktualisFutas(idopont, futok);
+
+  // Ellenörizzük hogy van e közelebb jutó ha van hozzáadjuk a FutoKi-hez
+  futoKi = kozelCheck(idopont, futok, futoKi);
+
+  // Csökkenő sorrendbe állítás
+  futok.sort(function (a, b) {
+    return b[0] - a[0];
+  });
+
+  // Aktuális Futo.Ki megjelenítése
+  console.log(`Futo.Ki: ${futoKi}`);
+}
+
+/* ---------- FUNCTIONS ---------- */
+
+// Futások távolságának megjelenítése
+function aktualisFutas(idopont, futok) {
+  console.log('---------- KÖVETKEZŐ IDŐPONT ----------');
+  console.log(`Időpont: ${idopont}`);
+  for (const i in futok) {
+    console.log(`[ ${futok[i][2]}. számú futó ] Távolsága: ${futok[i][0]}`);
+  }
+}
+
+// ha addig megy mig mindenki be nem ért
+function mindekiBeer(futok) {
+  let mindenkiZero = false;
+  ellenorzes: for (const i in futok) {
+    if (futok[i][0] > 0) {
+      mindenkiZero = true;
+      break ellenorzes;
+    }
+  }
+  return mindenkiZero;
+}
+
+function kozelCheck(idopont, futok, futoKi) {
+  // mindenkit mindennel megvizsgálunk
+  for (let i = 0; i < futok.length; i++) {
+    for (let x = i + 1; x < futok.length; x++) {
+      if ((futok[i][0] < futok[x][0]) && ((futok[i][0] > 0) && futok[x][0] > 0) && (futok[i][2] < futok[x][2])) {
+	console.log(`Időpont: ${idopont} = ${futok[i][0]} < ${futok[x][0]} emellett ${futok[i][2]} < ${futok[x][2]} `);
+        futoKi += `${idopont} `
+        console.log('Sorrend csere történt!');
+      }
+    }
+  }
+  return futoKi;
+}
